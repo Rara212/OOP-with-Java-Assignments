a2252d942adb27510fab70aa88bc730244fb0f1d
Mutya Mustafa <78282415+Rara212@users.noreply.github.com>
2022-05-07 10:44:09 +0700

    Overriding

---
+++ SoundsofAnimal/src/soundsofanimal/Cat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soundsofanimal;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cat extends SoundsofAnimal {
+     @Override
+     public void sound() {
+        System.out.println("Meaowwww");
+    }
+}
